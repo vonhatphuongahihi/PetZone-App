935d81927fc3c5ba10710322757df5403d43fcc2
Hoanggiaminh <hoanggiaminh27012004@gmail.com>
2025-10-23 22:55:26 +0700

    fix conflict

--- services/authService.ts
+++ services/authService.ts
@@ -1,6 +1,6 @@
 // Thay đổi localhost thành IP của máy tính 
 // http://192.168.1.100:3001/api
-const API_BASE_URL = 'http://10.76.31.127:3001/api';
+export const API_BASE_URL = 'http://10.76.31.127:3001/api';
 
 export interface RegisterData {
     email: string;
